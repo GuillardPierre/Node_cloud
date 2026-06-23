2dba63c3063e5f4886eee2c47d9ed663fc7ba7bc
GuillardPierre <pguillard95@gmail.com>
2026-06-23 16:49:11 +0200

    fix filter by hardness

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -10,20 +10,12 @@ export const readAll = async (req, res) => {
 };
 
 export const readByHarness = async (req, res) => {
-  if (!req.params.type) {
-    res.status(400).send("Type parameter is required");
-    return;
-  }
   try {
     const woods = await prisma.wood.findMany({
       where: {
-        type: req.params.type,
+        hardness: req.params.hardness,
       },
     });
-    if (woods.length === 0) {
-      res.status(404).send("No woods found for the specified type");
-      return;
-    }
     res.send(woods);
   } catch (error) {
     res.status(500).send("Error fetching woods by harness: " + error.message);
