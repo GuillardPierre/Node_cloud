66023963902c9afba2c03a02dd343519a974dd21
GuillardPierre <pguillard95@gmail.com>
2026-06-24 11:49:07 +0200

    Add login functionnality

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,5 @@
 import { prisma } from "../../app.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export const signup = async (req, res) => {
@@ -27,6 +28,38 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("You are login");
+export const login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password) {
+    res.status(400).send("Email and password are required");
+    return;
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (!user) {
+      res.status(404).send("User not found");
+      return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      res.status(401).send("Invalid password");
+      return;
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).send("Error during login: " + error.message);
+  }
 };
