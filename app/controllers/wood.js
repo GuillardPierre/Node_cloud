99c1c6202ea1f3d139e8a390dd5385141e54e838
GuillardPierre <pguillard95@gmail.com>
2026-06-25 10:36:51 +0200

    Add HATEOAS to woods roads

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,9 +1,30 @@
 import { prisma } from "../../app.js";
 
+const buildWoodLinks = (req, wood) => {
+  const baseUrl = `${req.protocol}://${req.get("host")}/api/woods`;
+  return {
+    self: {
+      href: `${baseUrl}/${wood.id}`,
+      rel: "self",
+      method: "GET",
+    },
+    same_hardness: {
+      href: `${baseUrl}/${wood.hardness}`,
+      rel: "same_hardness",
+      method: "GET",
+    },
+  };
+};
+
+const attachLinks = (req, wood) => ({
+  ...wood,
+  _links: buildWoodLinks(req, wood),
+});
+
 export const readAll = async (req, res) => {
   try {
     const woods = await prisma.wood.findMany();
-    res.send(woods);
+    res.send(woods.map((wood) => attachLinks(req, wood)));
   } catch (error) {
     res.status(500).send("Error fetching woods: " + error.message);
   }
@@ -16,16 +37,15 @@ export const readByHardness = async (req, res) => {
         hardness: req.params.hardness,
       },
     });
-    res.send(woods);
+    res.send(woods.map((wood) => attachLinks(req, wood)));
   } catch (error) {
-    res.status(500).send("Error fetching woods by harness: " + error.message);
+    res.status(500).send("Error fetching woods by hardness: " + error.message);
   }
 };
 
 export const create = async (req, res) => {
   try {
     const payload = req.body.data ? JSON.parse(req.body.data) : req.body;
-
     const { name, type, hardness } = payload;
     const image = req.file
       ? `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
@@ -45,7 +65,7 @@ export const create = async (req, res) => {
       },
     });
 
-    res.status(201).send(wood);
+    res.status(201).send(attachLinks(req, wood));
   } catch (error) {
     res.status(500).send("Error creating wood: " + error.message);
   }
